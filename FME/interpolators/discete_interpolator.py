fixed bug with gradient points and added visualisation of input data

FME/interpolators/discete_interpolator.py
@@ -43,6 +43,7 @@ class DiscreteInterpolator(GeologicalInterpolator):
             self.col.extend(idc.flatten().tolist())
             self.B.extend(B.flatten().tolist())
         if self.shape == 'square':
+            print('square')
             for i in range(4):
                 self.B[element[i]] += (p.val * c[i] * w)
                 for j in range(4):
@@ -92,9 +93,15 @@ class DiscreteInterpolator(GeologicalInterpolator):
             import eigensparse
             self.c[self.region] = eigensparse.lsqspqr(self.AA.tocsr(), self.B)
             return
-        if solver == 'lu' and self.shape == 'square':
-            lu = sla.splu(self.AA.tocsr())
-            b = self.B  # np.array([1, 2, 3, 4])
+        if solver == 'lu':
+            if self.shape == 'rectangular':
+                A = self.AA.T.dot(self.AA)
+                B = self.AA.T.dot(self.B)
+            if self.shape == 'square':
+                A = self.AA
+                B = self.B
+            lu = sla.splu(A)
+            b = B  # np.array([1, 2, 3, 4])
             self.c[self.region] = lu.solve(b)
             if clear:
                 self.AA = None
@@ -103,14 +110,20 @@ class DiscreteInterpolator(GeologicalInterpolator):
                 self.row = []
                 lu = None
             return
-        if solver == 'chol' and self.shape == 'square':
+        if solver == 'chol':
             try:
                 from sksparse.cholmod import cholesky
             except ImportError:
                 print("Scikit Sparse not installed try another solver e.g. lu")
                 return
-            factor = cholesky(self.AA.tocsc())
-            self.c = factor(self.B)
+            if self.shape == 'rectangular':
+                A = self.AA.T.dot(self.AA)
+                B = self.AA.T.dot(self.B)
+            if self.shape == 'square':
+                A = self.AA
+                B = self.B
+            factor = cholesky(A)
+            self.c = factor(B)
             if clear:
                 self.AA = None
                 self.A = []
@@ -118,5 +131,61 @@ class DiscreteInterpolator(GeologicalInterpolator):
                 self.row = []
                 factor = None
             return
+        if solver == 'cg':
+            if self.shape == 'rectangular':
+                A = self.AA.T @ self.AA
+                B = self.AA.T @ self.B
+            if self.shape == 'square':
+                A = self.AA
+                B = self.B
+            self.cc_ = sla.cg(A,B)
+        if solver == 'cgs':
+            if self.shape == 'rectangular':
+                A = self.AA.T.dot(self.AA)
+                B = self.AA.T.dot(self.B)
+            if self.shape == 'square':
+                A = self.AA
+                B = self.B
+            self.cc_ = sla.cgs(A,B)
+        if solver == 'bicg':
+            if self.shape == 'rectangular':
+                A = self.AA.T.dot(self.AA)
+                B = self.AA.T.dot(self.B)
+            if self.shape == 'square':
+                A = self.AA
+                B = self.B
+            self.cc_ = sla.bicg(A,B)
+        if solver == 'qmr':
+            if self.shape == 'rectangular':
+                A = self.AA.T.dot(self.AA)
+                B = self.AA.T.dot(self.B)
+            if self.shape == 'square':
+                A = self.AA
+                B = self.B
+            self.cc_ = sla.qmr(A,B)
+        if solver == 'gmres':
+            if self.shape == 'rectangular':
+                A = self.AA.T.dot(self.AA)
+                B = self.AA.T.dot(self.B)
+            if self.shape == 'square':
+                A = self.AA
+                B = self.B
+            self.cc_ = sla.gmres(A,B)
+        if solver == 'lgmres':
+            if self.shape == 'rectangular':
+                A = self.AA.T.dot(self.AA)
+                B = self.AA.T.dot(self.B)
+            if self.shape == 'square':
+                A = self.AA
+                B = self.B
+            self.cc_ = sla.lgmres(A,B)
+        if solver == 'minres':
+            if self.shape == 'rectangular':
+                A = self.AA.T.dot(self.AA)
+                B = self.AA.T.dot(self.B)
+            if self.shape == 'square':
+                A = self.AA
+                B = self.B
+            self.cc_ = sla.minres(A,B)
         self.c[self.region] = self.cc_[0]
         self.node_values = self.c

FME/interpolators/piecewiselinear_interpolator.py
@@ -89,7 +89,7 @@ class PiecewiseLinearInterpolator(DiscreteInterpolator):
         self.add_constraints_to_least_squares(A*w,B*w,idc)
         return
 
-    def add_gradient_ctr_pts(self, w=1.0):  # for now weight all gradient points the same
+    def add_gradient_ctr_pts(self, w=10.0):  # for now weight all gradient points the same
         """
         add gradient norm constraints to the interpolator
         :param w: weight per constraint
@@ -97,7 +97,7 @@ class PiecewiseLinearInterpolator(DiscreteInterpolator):
         """
         points = self.get_gradient_control()
         if points.shape[0] > 0:
-            e, inside = self.mesh.elements_for_array(points[:,3:])
+            e, inside = self.mesh.elements_for_array(points[:,:3])
             d_t = self.mesh.get_elements_gradients(e)
             points[:,3:] /= np.linalg.norm(points[:,3:],axis=1)[:,None]
             #add in the element gradient matrix into the inte
@@ -158,4 +158,4 @@ class PiecewiseLinearInterpolator(DiscreteInterpolator):
         return TetrahedralMeshScalarField.from_node_values(
             self.mesh,
             self.propertyname,
-            self.node_values)
+            self.c)

FME/modelling/scalar_field.py
@@ -37,8 +37,9 @@ class TetrahedralMeshScalarField:
         return self.mesh.evaluate_value(evaluation_points, self.property_name)
 
     def evaluate_gradient(self, evaluation_points):
-        return self.mesh.evaluate_gradient(evaluation_points, self.property_name)
-
+        if evaluation_points.shape[0]>0:
+            return self.mesh.evaluate_gradient(evaluation_points, self.property_name)
+        return np.zeros((0,3))
     def mean_property_value(self):
         return np.nanmean(self.mesh.properties[self.property_name])
 
@@ -67,7 +68,6 @@ class TetrahedralMeshScalarField:
         ##convert from memoryview to np array
         tri = np.array(tri)
         ntri = np.array(ntri)[0]
-        print(ntri)
         ##create a triangle indices array and initialise to -1
         tris = np.zeros((ntri, 3)).astype(int)
         tris[:, :] = -1
@@ -92,15 +92,17 @@ class TetrahedralMeshScalarField:
         crosses = crosses / (np.sum(crosses ** 2, axis=1) ** (0.5))[:, np.newaxis]
         # get barycentre of faces and findproperty gradient from scalar field
         tribc = np.mean(tri[:ntri, :, :], axis=1)
-        # tribc = tribc[np.any(np.isnan(tribc), axis=1)]
-        # propertygrad = self.evaluate_gradient(tribc)
-        # propertygrad /= np.linalg.norm(propertygrad, axis=1)[:, None]
-        #
-        # # dot product between gradient and normal indicates if faces are incorrectly ordered
-        # dotproducts = (propertygrad * crosses[np.any(np.isnan(tribc), axis=1)]).sum(axis=1)
-        # # if dot product > 0 then adjust triangle indexing
-        # indices = (dotproducts > 0).nonzero()[0]
-        # tris[indices] = tris[indices, ::-1]
+        #tribc = tribc[np.any(np.isnan(tribc), axis=0)]
+        print(tribc)
+
+        propertygrad = self.evaluate_gradient(tribc)
+        propertygrad /= np.linalg.norm(propertygrad, axis=1)[:, None]
+
+        # dot product between gradient and normal indicates if faces are incorrectly ordered
+        dotproducts = (propertygrad * crosses).sum(axis=1)
+        # if dot product > 0 then adjust triangle indexing
+        indices = (dotproducts > 0).nonzero()[0]
+        tris[indices] = tris[indices, ::-1]
         #
         #
         nodes_np = np.zeros((n, 3))

FME/visualisation/model_visualisation.py
@@ -1,4 +1,5 @@
 import lavavu
+import numpy as np
 import meshpy
 # class ModelViewer():
 #     def __init__(self,modelsupport,**kwargs):
@@ -21,6 +22,11 @@ import meshpy
 #     def save_state(self,**kwargs):
 #
 #     def load_state(self,**kwargs):
+def surface_cutter(feature,isovalue,nodes,tris):
+    values = feature.evaluate_value(nodes)
+    property_bool = values > isovalue
+    tri_type_index = np.sum(property_bool[tris]*np.array([1,2,4]),axis=1)
+
 
 
 class LavaVuModelViewer:
@@ -54,6 +60,7 @@ class LavaVuModelViewer:
                 isovalue = kwargs['isovalue']
 
             tris, nodes = geological_feature.support.slice(isovalue)
+
             # reg = np.zeros(self.properties[propertyname].shape).astype(bool)
             # reg[:] = True
             # if 'region' in kwargs:
@@ -65,6 +72,7 @@ class LavaVuModelViewer:
             surf.vertices(nodes)
             surf.indices(tris)
             surf.colours(colour)
+
     def plot_structural_frame_isosurface(self,structural_frame,i, **kwargs):
         mean_property_val = structural_frame.supports[i].mean_property_value()
         min_property_val = structural_frame.supports[i].min_property_value()
@@ -90,7 +98,7 @@ class LavaVuModelViewer:
             # reg[:] = True
             # if 'region' in kwargs:
             #     reg = self.regions[kwargs['region']]
-            name = structural_frame.name + '_iso_%f' % isovalue
+            name = structural_frame.name + '_%i_iso_%f' % (i,isovalue)
             if 'name' in kwargs:
                 name = kwargs['name']
             surf = self.lv.triangles(name)
@@ -99,10 +107,6 @@ class LavaVuModelViewer:
             surf.colours(colour)
 
     def lv_plot_vector_field(self, propertyname, lv, **kwargs):
-        try:
-            import lavavu
-        except ImportError:
-            print("Cannot import Lavavu")
         if 'colour' not in kwargs:
             kwargs['colour'] = 'black'
         vectorslicing = 100
@@ -119,5 +123,15 @@ class LavaVuModelViewer:
     def plot_points(self, points, name, col='red'):
         p = self.lv.points(name, pointsize=4, pointtype="sphere", colour=col)
         p.vertices(points)
+
+    def plot_vector_data(self, position, vector, name, **kwargs):
+        if 'colour' not in kwargs:
+            kwargs['colour'] = 'black'
+        # normalise
+        vector /= np.linalg.norm(vector, axis=1)[:, None]
+        vectorfield = self.lv.vectors(name, **kwargs)
+        vectorfield.vertices(position)
+        vectorfield.vectors(vector)
+        return
 
 

tests/test_02.py
@@ -85,9 +85,6 @@ viewer.plot_isosurface(faulted_feature.hw_feature,isovalue=0)
 viewer.plot_isosurface(faulted_feature.fw_feature,isovalue=0)
 mask = fault_frame.supports[0].get_node_values() > 0
 mask[mesh.elements] = np.any(mask[mesh.elements] == True, axis=1)[:, None]
-viewer.plot_points(mesh.nodes[mask],"nodes",col="red")
-#print(mask)
+viewer.plot_points(mesh.nodes[mask], "nodes", col="red")
 viewer.plot_structural_frame_isosurface(fault_frame, 0, isovalue=0, colour='blue')
-# viewer.plot_structural_frame_isosurface(fault_frame,0,isovalue=2)
-#
 viewer.lv.interactive()