fe9462fcf00ec4b2c82f9e38a02e3d0098025afa
Lachlan Grose <lachlan.grose@monash.edu>
2019-08-30 16:03:17 +1000

    started working on a map plotter

--- FME/visualisation/map_viewer.py
+++ FME/visualisation/map_viewer.py
@@ -1,12 +1,22 @@
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 #
-# class MapViewer:
-#     def __init__(self,size):
+# class MapView:
+#     def __init__(self,origin,maximum,**kwargs):
 #
-#     def plot_orientations(self):
+#     def add_data_to_plot(self,feature):
+#         """
+#         Adds the data associated to the feature to the plot
+#         Parameters
+#         ----------
+#         feature geological feature
 #
+#         Returns
+#         -------
+#
+#         """
+#         gradient_data = feature.interpolator.get_gradient_control()
+#         value_data = feature.interpolator.get_control_points()
+#         plt.plot(value_data[:,0],value_data[:,1],value_data[:,3])
 #     def plot_scalarfield(self):
 #
 #     def contour_feature(self):
-#
-#     def
