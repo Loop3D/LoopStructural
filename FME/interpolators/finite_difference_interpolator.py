Updating tutorials again, fixed bug with dsi caused by normalising both the tetra grad and the constraint grad

FME/interpolators/finite_difference_interpolator.py
@@ -92,13 +92,13 @@ class FiniteDifferenceInterpolator(DiscreteInterpolator):
         points = self.get_gradient_control()
         if points.shape[0] > 0:
             # calculate unit vector for orientation data
-            points[:,3:]/=np.linalg.norm(points[:,3:],axis=1)[:,None]
+            # points[:,3:]/=np.linalg.norm(points[:,3:],axis=1)[:,None]
 
             node_idx, inside = self.support.position_to_cell_corners(points[:, :3])
             # calculate unit vector for node gradients
             # this means we are only constraining direction of grad not the magnitude
             T = self.support.calcul_T(points[inside, :3])
-            T /= np.linalg.norm(T,axis=1)[:,None,:]
+            # T /= np.linalg.norm(T,axis=1)[:,None,:]
             w /= 3
             self.add_constraints_to_least_squares(T[:, 0, :]*w, points[inside, 3]*w, node_idx[inside, :])
             self.add_constraints_to_least_squares(T[:, 1, :]*w, points[inside, 4]*w, node_idx[inside, :])

FME/interpolators/piecewiselinear_interpolator.py
@@ -18,7 +18,6 @@ class PiecewiseLinearInterpolator(DiscreteInterpolator):
         the linear equations on
         'propertyname' the name of the property that is interpolated on the mesh
         """
-        self.region = "everywhere"
         # whether to assemble a rectangular matrix or a square matrix
         self.shape = 'rectangular'
         self.support = mesh
@@ -95,11 +94,11 @@ class PiecewiseLinearInterpolator(DiscreteInterpolator):
             vecs = nodes[:,1:,:] - nodes[:,0,None,:]
             vol = np.abs(np.linalg.det(vecs)) #/ 6
             d_t = self.support.get_elements_gradients(e)
-            d_t /= np.linalg.norm(d_t,axis=1)[:,None, :]
+            #d_t /= np.linalg.norm(d_t,axis=1)[:,None, :]
 
             # print()
             d_t *= vol[:,None,None]
-            points[:,3:] /= np.linalg.norm(points[:,3:],axis=1)[:,None]
+            #points[:,3:] /= np.linalg.norm(points[:,3:],axis=1)[:,None]
             # add in the element gradient matrix into the inte
             e=np.tile(e,(3,1)).T
             idc = self.support.elements[e]