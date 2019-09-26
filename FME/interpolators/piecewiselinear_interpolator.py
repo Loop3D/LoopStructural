changed constant gradient to use normalised face normal. This should fix up problems with CG values being too high

FME/interpolators/piecewiselinear_interpolator.py
@@ -95,11 +95,14 @@ class PiecewiseLinearInterpolator(DiscreteInterpolator):
             vecs = nodes[:,1:,:] - nodes[:,0,None,:]
             vol = np.abs(np.linalg.det(vecs)) #/ 6
             d_t = self.support.get_elements_gradients(e)
-            #d_t /= np.linalg.norm(d_t,axis=1)[:,None, :]
+            d_t /= np.linalg.norm(d_t,axis=1)[:,None, :]
 
             # print()
             d_t *= vol[:,None,None]
-            #points[:,3:] /= np.linalg.norm(points[:,3:],axis=1)[:,None]
+            w*=10^11
+
+            points[:,3:] /= np.linalg.norm(d_t,axis=1)[:,None, :]#np.linalg.norm(points[:,3:],axis=1)[:,None]
+
             # add in the element gradient matrix into the inte
             e=np.tile(e,(3,1)).T
             idc = self.support.elements[e]
@@ -136,10 +139,11 @@ class PiecewiseLinearInterpolator(DiscreteInterpolator):
             vecs = nodes[:, 1:, :] - nodes[:, 0, None, :]
             vol = np.abs(np.linalg.det(vecs)) / 6
             A = self.support.calc_bary_c(e, points[:, :3])
-            A *= vol[None,:]
+            #A *= vol[None,:]
+            #w /= 2059581*10**11
             idc = self.support.elements[e]
             # w /= points.shape[0]
-            self.add_constraints_to_least_squares(A.T*w,points[:,3]*w*vol,idc)
+            self.add_constraints_to_least_squares(A.T*w, points[:, 3]*w, idc)
 
     def add_gradient_orthogonal_constraint(self, elements, normals, w=1.0, B=0):
         """