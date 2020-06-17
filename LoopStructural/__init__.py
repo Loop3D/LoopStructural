31016a2d90f3c8b06311726172636b80d886e33f
Lachlan Grose <lachlan.grose@monash.edu>
2020-06-17 19:22:13 +1000

    (BUGFIX) updated logging

--- LoopStructural/__init__.py
+++ LoopStructural/__init__.py
@@ -19,6 +19,20 @@ from logging.config import dictConfig
 from .modelling.core.geological_model import GeologicalModel
 from .visualisation.model_visualisation import LavaVuModelViewer
 from .visualisation.map_viewer import MapView
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                    datefmt='%m-%d %H:%M',
+                    filename='loop_structural_log.log',
+                    filemode='w')
+# define a Handler which writes INFO messages or higher to the sys.stderr
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+# add the handler to the root logger
+logging.getLogger().addHandler(console)
+
+
+
 # logging_config = dict(
 #     version=1,
 #     formatters={
