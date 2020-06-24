(BUGFIX) fixing bug with gradient constraints where gradient was not being honoured. Due to messing up assembly of least squares system

LoopStructural/interpolators/finite_difference_interpolator.py
@@ -203,8 +203,8 @@ class FiniteDifferenceInterpolator(DiscreteInterpolator):
             A = np.einsum('ij,ijk->ik', strike_vector.T, T)
 
             B = np.zeros(points[inside, :].shape[0])
-            self.add_constraints_to_least_squares(A * w, B, idc[inside, :])
-            A = np.einsum('ij,ijk->ik', dip_vector.T, T)
+            # self.add_constraints_to_least_squares(A * w, B, idc[inside, :])
+            A += np.einsum('ij,ijk->ik', dip_vector.T, T)
             self.add_constraints_to_least_squares(A * w, B, idc[inside, :])
 
     def add_norm_constraint(self, w=1.):

LoopStructural/interpolators/piecewiselinear_interpolator.py
@@ -154,12 +154,12 @@ class PiecewiseLinearInterpolator(DiscreteInterpolator):
             idc = gi[tetras]
             B = np.zeros(idc.shape[0])
             outside = ~np.any(idc == -1, axis=1)
-            self.add_constraints_to_least_squares(A[outside, :] * w,
-                                                  B[outside], idc[outside, :],
-                                                  name = 'gradient')
-            A = np.einsum('ji,ijk->ik', dip_vector, element_gradients)
-            A *= vol[:, None]
-
+            # self.add_constraints_to_least_squares(A[outside, :] * w,
+            #                                       B[outside], idc[outside, :],
+            #                                       name = 'gradient')
+            A2 = np.einsum('ji,ijk->ik', dip_vector, element_gradients)
+            A2 *= vol[:, None]
+            A+=A2
             self.add_constraints_to_least_squares(A[outside, :] * w,
                                           B[outside], idc[outside, :],
                                                   name='gradient')