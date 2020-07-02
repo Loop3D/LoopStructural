(BUG) trying to fix issues with shallowly dipping units defiend only on surface

LoopStructural/modelling/core/geological_model.py
@@ -205,7 +205,10 @@ class GeologicalModel:
                 self.data.loc[mask, 'strike'], self.data.loc[mask, 'dip'])
             self.data.drop(['strike', 'dip'], axis=1, inplace=True)
         #     self.data.loc
-
+        # if 'nx' in self.data and 'ny' in self.data and 'nz' in self.data:
+        #     mask = np.all(~np.isnan(self.data.loc[:, ['nx', 'ny','nz']]),
+        #                   axis=1)
+        #     self.data.loc[mask,['nx', 'ny','nz']] /= self.scale_factor
     def extend_model_data(self, newdata):
         """
         Extends the data frame
@@ -289,16 +292,21 @@ class GeologicalModel:
         # add a buffer to the interpolation domain, this is necessary for
         # faults but also generally a good
         # idea to avoid boundary problems
+        # buffer = bb[1, :]
+        buffer = (bb[1,:]-bb[0,:])*buffer
         bb[0, :] -= buffer  # *(bb[1,:]-bb[0,:])
         bb[1, :] += buffer  # *(bb[1,:]-bb[0,:])
+        box_vol = (bb[1, 0]-bb[0, 0]) * (bb[1, 1]-bb[0, 1]) * (bb[1, 2]-bb[0, 2])
         if interpolatortype == "PLI":
             nelements /= 5
-            ele_vol = bb[1, 0] * bb[1, 1] * bb[1, 2] / nelements
+            ele_vol = box_vol / nelements
             # calculate the step vector of a regular cube
             step_vector = np.zeros(3)
             step_vector[:] = ele_vol ** (1. / 3.)
+            # step_vector /= np.array([1,1,2])
             # number of steps is the length of the box / step vector
-            nsteps = ((bb[1, :] - bb[0, :]) / step_vector).astype(int)
+            nsteps = np.ceil((bb[1, :] - bb[0, :]) / step_vector).astype(int)
+            print(nsteps)
             # create a structured grid using the origin and number of steps
             mesh_id = 'mesh_{}'.format(nelements)
             mesh = self.support.get(mesh_id,
@@ -313,12 +321,12 @@ class GeologicalModel:
 
         if interpolatortype == 'FDI':
             # find the volume of one element
-            ele_vol = bb[1, 0] * bb[1, 1] * bb[1, 2] / nelements
+            ele_vol = box_vol / nelements
             # calculate the step vector of a regular cube
             step_vector = np.zeros(3)
             step_vector[:] = ele_vol ** (1. / 3.)
             # number of steps is the length of the box / step vector
-            nsteps = ((bb[1, :] - bb[0, :]) / step_vector).astype(int)
+            nsteps = np.ceil((bb[1, :] - bb[0, :]) / step_vector).astype(int)
             # create a structured grid using the origin and number of steps
             grid_id = 'grid_{}'.format(nelements)
             grid = self.support.get(grid_id, StructuredGrid(origin=bb[0, :],
@@ -332,12 +340,12 @@ class GeologicalModel:
 
         if interpolatortype == "DFI":  # "fold" in kwargs:
             nelements /= 5
-            ele_vol = bb[1, 0] * bb[1, 1] * bb[1, 2] / nelements
+            ele_vol = box_vol / nelements
             # calculate the step vector of a regular cube
             step_vector = np.zeros(3)
             step_vector[:] = ele_vol ** (1. / 3.)
             # number of steps is the length of the box / step vector
-            nsteps = ((bb[1, :] - bb[0, :]) / step_vector).astype(int)
+            nsteps = np.ceil((bb[1, :] - bb[0, :]) / step_vector).astype(int)
             # create a structured grid using the origin and number of steps
             mesh = kwargs.get('mesh', TetMesh(origin=bb[0, :], nsteps=nsteps,
                                               step_vector=step_vector))

LoopStructural/utils/map2loop.py
@@ -56,10 +56,10 @@ def process_map2loop(m2l_directory, flags={}):
     # convert azimuth and dip to gx, gy, gz
     from LoopStructural.utils.helper import strike_dip_vector
     contact_orientations['strike'] = contact_orientations['azimuth'] - 90
-    contact_orientations['gx'] = np.nan
-    contact_orientations['gy'] = np.nan
-    contact_orientations['gz'] = np.nan
-    contact_orientations[['gx', 'gy', 'gz']] = strike_dip_vector(contact_orientations['strike'],
+    contact_orientations['nx'] = np.nan
+    contact_orientations['ny'] = np.nan
+    contact_orientations['nz'] = np.nan
+    contact_orientations[['nx', 'ny', 'nz']] = strike_dip_vector(contact_orientations['strike'],
                                                                  contact_orientations['dip'])
     contact_orientations.drop(['strike', 'dip', 'azimuth'], inplace=True, axis=1)
 
@@ -72,7 +72,7 @@ def process_map2loop(m2l_directory, flags={}):
     stratigraphic_column = {}
     unit_id = 0
     val = {}
-    for i in groups['group number'].unique():
+    for i in groups['group number'].unique()[::-1]:
         g = supergroups[groups.loc[groups['group number'] == i, 'group'].iloc[0]]
         if g not in stratigraphic_column:
             stratigraphic_column[g] = {}
@@ -140,7 +140,7 @@ def process_map2loop(m2l_directory, flags={}):
             'bounding_box':bb,
             'strat_va':strat_val}
 
-def build_model(m2l_data, skip_faults = False, fault_params = None, foliation_params=None):
+def build_model(m2l_data, skip_faults = False, fault_params = None, foliation_params=None, rescale=True):
     """[summary]
 
     [extended_summary]
@@ -172,7 +172,7 @@ def build_model(m2l_data, skip_faults = False, fault_params = None, foliation_pa
     boundary_points[1, 1] = m2l_data['bounding_box']['maxy']
     boundary_points[1, 2] = m2l_data['bounding_box']['upper']
 
-    model = GeologicalModel(boundary_points[0, :], boundary_points[1, :])
+    model = GeologicalModel(boundary_points[0, :], boundary_points[1, :], rescale=rescale)
     model.set_model_data(m2l_data['data'])
     if not skip_faults:
         faults = []