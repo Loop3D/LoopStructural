f39f90c7bc4f824e73b8053f4e8239f59ce33762
Lachlan Grose <lachlan.grose@monash.edu>
2021-02-10 14:47:32 +1100

    (BUGFIX) fixed typo in test, why did it pass?

--- tests/unit_tests/modelling/test_structural_frame.py
+++ tests/unit_tests/modelling/test_structural_frame.py
@@ -1,5 +1,6 @@
 from LoopStructural.modelling.features import StructuralFrame, StructuralFrameBuilder, GeologicalFeature
 from LoopStructural import GeologicalModel
+import numpy as np
 def test_structural_frame():
     coordinate_0 = GeologicalFeature('coord0',None)
     coordinate_1 = GeologicalFeature('coord1',None)
@@ -8,7 +9,7 @@ def test_structural_frame():
     assert (frame != None)
     assert frame.name == 'structural_frame'
 def set_model():
-    model = GeologicalModel(np.zeros(),np.ones())
+    model = GeologicalModel(np.zeros(3),np.ones(3))
     coordinate_0 = GeologicalFeature('coord0',None)
     coordinate_1 = GeologicalFeature('coord1',None)
     coordinate_2 = GeologicalFeature('coord2',None)
