3dcbf7ec119aece13fc40ec0b1460f18d0afc1a8
Lachlan Grose <lachlan.grose@monash.edu>
2021-03-05 18:32:35 +1100

    (BUGFIX) fault that doesn't exist throws overprinting warning nsteps is used for regular grid default changed to 50,50,25

--- LoopStructural/modelling/core/geological_model.py
+++ LoopStructural/modelling/core/geological_model.py
@@ -77,7 +77,7 @@ class GeologicalModel:
 
 
     """
-    def __init__(self, origin, maximum, rescale=True, nsteps=(40, 40, 40),
+    def __init__(self, origin, maximum, rescale=True, nsteps=(50, 50, 25),
                  reuse_supports=False, logfile=None, loglevel='info'):
         """
         Parameters
@@ -1179,7 +1179,11 @@ class GeologicalModel:
         overprinted = kwargs.get('overprints', [])
         overprinted_faults = []
         for o in overprinted:
-            overprinted_faults.append(self.features[self.feature_name_index[o]])
+            try:
+                overprinted_faults.append(self.features[self.feature_name_index[o]])
+            except KeyError:
+                logger.warning("{} could not be added to overprint {} \
+                because it does not exist".format(o,fault_surface_data))
         self._add_faults(fault_frame_builder[0],overprinted_faults)
         self._add_faults(fault_frame_builder[1],overprinted_faults)
         self._add_faults(fault_frame_builder[2],overprinted_faults)
@@ -1252,7 +1256,7 @@ class GeologicalModel:
         return points
 
 
-    def regular_grid(self, nsteps=(50, 50, 25), shuffle = True, rescale=False):
+    def regular_grid(self, nsteps=None, shuffle = True, rescale=False):
         """
         Return a regular grid within the model bounding box
 
@@ -1266,6 +1270,8 @@ class GeologicalModel:
         xyz : np.array((N,3),dtype=float)
             locations of points in regular grid
         """
+        if nsteps is None:
+            nsteps = self.nsteps
         x = np.linspace(self.bounding_box[0, 0], self.bounding_box[1, 0],
                         nsteps[0])
         y = np.linspace(self.bounding_box[0, 1], self.bounding_box[1, 1],
