1966997abd83810830a3970f4e43747d3020e545
Roy Thomson <Roy.Thomson@monash.edu>
2021-03-10 17:13:17 +1100

    (BUG) Fix dataframe append issue when appending when len(DataFrame) index is already present

--- LoopStructural/modelling/fault/fault_builder.py
+++ LoopStructural/modelling/fault/fault_builder.py
@@ -1,5 +1,6 @@
 from ..features.structural_frame_builder import StructuralFrameBuilder
 
+import pandas as pd
 import numpy as np
 class FaultBuilder(StructuralFrameBuilder):
     def __init__(self,interpolator=None,interpolators=None,model=None,fault_bounding_box_buffer=0.2,**kwargs):
@@ -75,35 +76,47 @@ class FaultBuilder(StructuralFrameBuilder):
                 fault_edges[0,:] = fault_center[:3]+normal_vector*influence_distance
                 fault_edges[1,:] = fault_center[:3]-normal_vector*influence_distance
                 self.update_geometry(fault_edges)
-                data.loc[len(data),['X','Y','Z','feature_name','val','coord']] = \
-                    [fault_edges[0,0],fault_edges[0,1],fault_edges[0,2],self.name,1,0]
-                data.loc[len(data),['X','Y','Z','feature_name','val','coord']] = \
-                    [fault_edges[1,0],fault_edges[1,1],fault_edges[1,2],self.name,-1,0]
+                data = data.append(pd.DataFrame(
+                    [[fault_edges[0,0],fault_edges[0,1],fault_edges[0,2],self.name,1,0]],
+                    columns=['X','Y','Z','feature_name','val','coord']),
+                    ignore_index=True)
+                data = data.append(pd.DataFrame(
+                    [[fault_edges[1,0],fault_edges[1,1],fault_edges[1,2],self.name,-1,0]],
+                    columns=['X','Y','Z','feature_name','val','coord']),
+                    ignore_index=True)
             if horizontal_radius is not None:
                 fault_tips[0,:] = fault_center[:3]+strike_vector*horizontal_radius
                 fault_tips[1,:] = fault_center[:3]-strike_vector*horizontal_radius
                 self.update_geometry(fault_tips)
-                data.loc[len(data),['X','Y','Z','feature_name','val','coord']] = \
-                    [fault_center[0],fault_center[1],fault_center[2],self.name,0,2]
+                data = data.append(pd.DataFrame(
+                    [[fault_center[0],fault_center[1],fault_center[2],self.name,0,2]],
+                    columns=['X','Y','Z','feature_name','val','coord']),
+                    ignore_index=True)
                 # data.loc[len(data),['X','Y','Z','feature_name','val','coord']] = \
                 #     [fault_tips[1,0],fault_tips[1,1],fault_tips[1,2],self.name,-1,2]
                 strike_vector /= horizontal_radius
             if vertical_radius is not None:
                 fault_depth[0,:] = fault_center[:3]+slip_vector*vertical_radius
                 fault_depth[1,:] = fault_center[:3]-slip_vector*vertical_radius
-                data.loc[len(data),['X','Y','Z','feature_name','val','coord']] = \
-                    [fault_center[0],fault_center[1],fault_center[2],self.name,0,1]
+                data = data.append(pd.DataFrame(
+                    [[fault_center[0],fault_center[1],fault_center[2],self.name,0,1]],
+                    columns=['X','Y','Z','feature_name','val','coord']),
+                    ignore_index=True)
                 # data.loc[len(data),['X','Y','Z','feature_name','val','coord']] = \
                 #     [fault_depth[1,0],fault_depth[1,1],fault_depth[1,2],self.name,-1,1]
                 self.update_geometry(fault_depth)
                 #TODO need to add data here
                 slip_vector /= vertical_radius
 
-            data.loc[len(data),['X','Y','Z','feature_name','nx','ny','nz','val','coord']] =\
-                [fault_center[0],fault_center[1],fault_center[2],self.name,slip_vector[0],slip_vector[1],slip_vector[2],0,1]
+            data = data.append(pd.DataFrame(
+                [[fault_center[0],fault_center[1],fault_center[2],self.name,slip_vector[0],slip_vector[1],slip_vector[2],0,1]],
+                columns=['X','Y','Z','feature_name','nx','ny','nz','val','coord']),
+                ignore_index=True)
         # add strike vector to constraint fault extent
-            data.loc[len(data),['X','Y','Z','feature_name','nx','ny','nz','coord']] = [fault_center[0],fault_center[1],fault_center[2],\
-                self.name, strike_vector[0], strike_vector[1], strike_vector[2], 2]
+            data = data.append(pd.DataFrame(
+                [[fault_center[0],fault_center[1],fault_center[2],self.name,strike_vector[0],strike_vector[1],strike_vector[2],2]],
+                columns=['X','Y','Z','feature_name','nx','ny','nz','coord']),
+                ignore_index=True)
         self.add_data_from_data_frame(data)
     def set_mesh_geometry(self,buffer):
         """set the mesh geometry
