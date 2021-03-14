513abad3f2b85b9490ca6f03a462395e5063e210
lachlangrose <lachlan.grose@monash.edu>
2021-03-14 16:49:47 +1100

    Update __init__.py

--- LoopStructural/__init__.py
+++ LoopStructural/__init__.py
@@ -29,7 +29,7 @@ formatter = logging.Formatter('%(asctime)s ~ %(name)-12s ~ %(levelname)-10s ~ %(
 ch.setFormatter(formatter)
 ch.setLevel(logging.WARNING)
 loggers = {}
-__version__ = '1.0.86a'
+__version__ = '1.0.87'
 from .modelling.core.geological_model import GeologicalModel
 from .utils import log_to_console, log_to_file, getLogger
 logger = getLogger(__name__)
