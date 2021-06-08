87cac92af70dde37f968e6dd9debd5a1558bf961
Lachlan Grose <lachlan.grose@monash.edu>
2021-06-08 13:54:58 +1000

    updating documentation to have changelog

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,6 +12,7 @@
 #
 import os
 import sys
+# import m2r
 sys.path.insert(0, os.path.abspath('../..'))
 
 
@@ -49,8 +50,10 @@ extensions = [
         # The Napoleon extension allows for nicer argument formatting.
         'sphinx.ext.napoleon',
         # add sphinx gallery
-        'sphinx_gallery.gen_gallery'
+        'sphinx_gallery.gen_gallery',
         # citations
+        'myst_parser'
+
 
 ]
 
