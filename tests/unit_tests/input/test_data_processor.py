affa380b8fcb47c63074f1cb3baa110c670ad5d2
Lachlan Grose <lachlan.grose@gmail.com>
2021-08-10 14:15:30 +1000

    CI removing test that wasn't complete

--- tests/unit_tests/input/test_data_processor.py
+++ tests/unit_tests/input/test_data_processor.py
@@ -2,19 +2,19 @@ from LoopStructural.modelling import ProcessInputData
 import pandas as pd
 import numpy as np
 
-contacts - 
-contacts=None
-contact_orientations = None
-stratigraphic_order = None
-fault_orientations = None 
-fault_locations = None
-fault_properties = None 
-fault_edges = None
-intrusions = None
-fault_stratigraphy = None
-thicknesses = None
-colours = None
+# contacts - 
+# contacts=None
+# contact_orientations = None
+# stratigraphic_order = None
+# fault_orientations = None 
+# fault_locations = None
+# fault_properties = None 
+# fault_edges = None
+# intrusions = None
+# fault_stratigraphy = None
+# thicknesses = None
+# colours = None
 
-def test_create_processor():
-    contacts = np.zeros(
-    ProcessInputData(
+# def test_create_processor():
+#     contacts = np.zeros(
+#     ProcessInputData(
