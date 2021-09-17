a782f632f8cd5dd2f01e7be3a40c4ba2dc5417ec
Lachlan Grose <lachlan.grose@gmail.com>
2021-09-17 12:02:27 +1000

    fix: adding intrusion module

---
+++ LoopStructural/modelling/intrusions/__init__.py
@@ -0,0 +1,1 @@
+from .intrusion_frame import IntrusionFrame
---
+++ LoopStructural/modelling/intrusions/intrusion_frame.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+from LoopStructural.modelling.features.structural_frame import StructuralFrame
+
+from LoopStructural.utils import getLogger
+logger = getLogger(__name__)
+
+
+class IntrusionFrame(StructuralFrame):
+    def __init__(self, name, features): 
+        StructuralFrame.__init__(self, name, features)
+        logger.warning("There is an error")
+
