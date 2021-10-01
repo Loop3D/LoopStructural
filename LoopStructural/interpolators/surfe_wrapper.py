af998bfddd09c3654d33e1123601992607cd2885
Lachlan Grose <lachlan.grose@gmail.com>
2021-10-01 15:06:09 +1000

    doc: adding docstrings for surfe

--- LoopStructural/interpolators/surfe_wrapper.py
+++ LoopStructural/interpolators/surfe_wrapper.py
@@ -26,8 +26,8 @@ class SurfeRBFInterpolator(GeologicalInterpolator):
         if method == 'single_surface':
             logger.info("Using single surface interpolator")
             self.surfe = surfepy.Surfe_API(1)
-        if method == 'Laujaunie':
-            logger.info("Using Laujaunie method")
+        if method == 'Lajaunie' or method='increments':
+            logger.info("Using Lajaunie method")
             self.surfe = surfepy.Surfe_API(2)
         if method == 'horizons':
             logger.info("Using surfe horizon")
@@ -68,6 +68,28 @@ class SurfeRBFInterpolator(GeologicalInterpolator):
         self.surfe.ComputeInterpolant()
 
     def _setup_interpolator(self, **kwargs):
+        """
+        Setup the interpolator
+
+        Parameters
+        ----------
+        kernel: str
+            kernel for interpolation r3, r, Gaussian, Multiquadrics, Inverse Multiquadrics
+            Thin Plate Spline, WendlandC2, MaternC4
+        regression: float
+            smoothing parameter default 0
+        greedy: tuple
+            greedy parameters first is interface threshold, second is angular threshold
+             default (0,0)
+        poly_order: int
+            order of the polynomial used for interpolation, default 1
+        radius: float
+            radius of the kernel, default None but required for SPD kernels
+        anisotropy: bool
+            apply global anisotropy from eigenvectors of orientation constraints, default False
+        
+        
+        """
         self.add_gradient_ctr_pts()
         self.add_norm_ctr_pts()
         self.add_ctr_pts()
@@ -103,6 +125,18 @@ class SurfeRBFInterpolator(GeologicalInterpolator):
         return self.surfe.InterpolantComputed()
 
     def evaluate_value(self, evaluation_points):
+        """Evaluate surfe interpolant at points
+
+        Parameters
+        ----------
+        evaluation_points : array of locations N,3
+            xyz of locations to evaluate
+
+        Returns
+        -------
+        np.array (N)
+            value of interpolant at points
+        """
         evaluation_points = np.array(evaluation_points)
         evaluated = np.zeros(evaluation_points.shape[0])
         mask = np.any(evaluation_points == np.nan, axis=1)
@@ -112,6 +146,19 @@ class SurfeRBFInterpolator(GeologicalInterpolator):
                 evaluation_points[~mask])
         return evaluated
     def evaluate_gradient(self, evaluation_points):
+        """Evaluate surfe interpolant gradient at points
+
+        Parameters
+        ----------
+        evaluation_points : array of locations N,3
+            xyz of locations to evaluate
+
+        Returns
+        -------
+        np.array (N,3)
+            gradient of interpolant at points
+        
+        """
         evaluation_points = np.array(evaluation_points)
         evaluated = np.zeros(evaluation_points.shape)
         mask = np.any(evaluation_points == np.nan, axis=1)
