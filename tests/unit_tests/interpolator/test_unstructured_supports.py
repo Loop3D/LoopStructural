3f718d9c178d13f1f8d382954331b598bd4a7b26
Lachlan Grose <lachlan.grose@gmail.com>
2021-10-11 20:35:03 +1100

    tests: adding test for unstructured mesh

---
+++ tests/unit_tests/interpolator/test_unstructured_supports.py
@@ -0,0 +1,41 @@
+import numpy as np
+from LoopStructural.interpolators.unstructured_tetra import UnStructuredTetMesh
+from os.path import dirname
+file_path = dirname(__file__)
+
+def test_get_elements():
+    nodes = np.loadtxt('{}/nodes.txt'.format(file_path))
+    elements = np.loadtxt('{}/elements.txt'.format(file_path))
+    elements=np.array(elements,dtype='int64')
+    neighbours = np.loadtxt('{}/neighbours.txt'.format(file_path))
+    
+    mesh = UnStructuredTetMesh(nodes,elements,neighbours)
+    points = np.random.random((100,3))
+    verts, c, tetra, inside = mesh.get_tetra_for_location(points)
+
+    vertices = nodes[elements,:]
+    pos = points[:,:]
+    vap = pos[None,:, :] - vertices[:, None, 0, :]
+    vbp = pos[None,:, :] - vertices[:, None, 1, :]
+
+    vab = vertices[:, None, 1, :] - vertices[:,None,  0, :]
+    vac = vertices[:, None, 2, :] - vertices[:,None,  0, :]
+    vad = vertices[:, None, 3, :] - vertices[:,None,  0, :]
+    vbc = vertices[:, None, 2, :] - vertices[:,None,  1, :]
+    vbd = vertices[:, None, 3, :] - vertices[:, None, 1, :]
+
+    va = np.einsum('ikj, ikj->ik', vbp, np.cross(vbd, vbc, axisa=2, axisb=2)) / 6.
+    vb = np.einsum('ikj, ikj->ik', vap, np.cross(vac, vad, axisa=2, axisb=2)) / 6.
+    vc = np.einsum('ikj, ikj->ik', vap, np.cross(vad, vab, axisa=2, axisb=2)) / 6.
+    vd = np.einsum('ikj, ikj->ik', vap, np.cross(vab, vac, axisa=2, axisb=2)) / 6.
+    v = np.einsum('ikj, ikj->ik', vab, np.cross(vac, vad, axisa=2, axisb=2)) / 6.
+
+    c = np.zeros((va.shape[0], pos.shape[0], 4))
+    c[:,:,  0] = va / v
+    c[:,:,  1] = vb / v
+    c[:,:,  2] = vc / v
+    c[:,:,  3] = vd / v
+    row,col = np.where(np.all(c>=0,axis=2))
+    tetra_idx = np.sort(row)
+
+    assert np.sum(tetra_idx-np.sort(tetra)) == 0
