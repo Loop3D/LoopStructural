fix: removing sign change for rotation angle

LoopStructural/modelling/fold/foldframe.py
@@ -151,10 +151,10 @@ class FoldFrame(StructuralFrame):
             # and 90
             vv = np.cross(s1g, s0g, axisa=1, axisb=1)
             ds = np.einsum('ij,ij->i', fold_axis, vv)
-            flr = np.where(np.abs(ds) > 0.5, np.rad2deg(np.arcsin(r2)),
-                           (- np.rad2deg(np.arcsin(r2))))
-            flr = np.where(flr < -90, (180. + flr), flr)
-            flr = np.where(flr > 90, -(180. - flr), flr)
+            flr = np.rad2deg(np.arcsin(r2))#np.where(np.abs(ds) < 0.5, np.rad2deg(np.arcsin(r2)),
+                          # (- np.rad2deg(np.arcsin(r2))))
+            # flr = np.where(flr < -90, (180. + flr), flr)
+            # flr = np.where(flr > 90, -(180. - flr), flr)
             return flr, s1
 
     def calculate_intersection_lineation(self, feature_builder):