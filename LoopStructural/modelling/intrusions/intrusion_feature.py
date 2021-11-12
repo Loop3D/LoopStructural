cbbf4e298960d19b976c5f533b642dcc7c54e839
Fer071989 <fernanda.alvaradoneves@monash.edu>
2021-11-12 18:40:41 +1100

    fix: optimization of evaluate function
    use of numpy vectorization

--- LoopStructural/modelling/intrusions/intrusion_feature.py
+++ LoopStructural/modelling/intrusions/intrusion_feature.py
@@ -43,6 +43,7 @@ class IntrusionFeature():
 
         self.intrusion_indicator_function = None
         self.evaluated_points = None
+    
     def min(self):
         
         if self.model is None:
@@ -78,15 +79,142 @@ class IntrusionFeature():
 
     def set_intrusion_body(self, intrusion_body):
         self.intrusion_feature_body= intrusion_body
-
+    
     def evaluate_value(self, points):
     
-        #---> parameters
-        # points (array x,y,z)= points where the intrusion is evaluated. Usually grid points to build the intrusion
-        # simulation_g_data = simulated thresholds for G
-        # simulation_s_data =  simulated thresholds for S
-        # intrusions frames and intrusio frame models
+        """
+        points : array (x,y,z),  points where the intrusion is evaluated.
+
+        """
+
+        simulation_g_data = self.simulation_growth_data
+        simulation_s_data = self.simulation_lateral_data
+        inet_polygon = None
+        intrusion_frame = self.structural_frame
+        model = self.model
         
+        #---> returns indicator function and scalar field with isovalue 0 = intrusion boundary
+
+        #compute coordinates values for each evaluated point
+        intrusion_coord0_pts = intrusion_frame[0].evaluate_value(points)
+        intrusion_coord1_pts = intrusion_frame[1].evaluate_value(points)
+        intrusion_coord2_pts = intrusion_frame[2].evaluate_value(points)
+        
+        #------ lateral extent thresholds for each of the evaluated points -------------
+
+        # if lateral extent values were simulated
+        if simulation_s_data is None:
+            print('No simultion for lateral extent')
+        else:
+            simulation_s_data.sort_values(['coord1'], ascending = [True], inplace = True)
+            
+            # containers for thresholds
+            s_minside_threshold = np.zeros(len(intrusion_coord1_pts))
+            s_maxside_threshold = np.zeros(len(intrusion_coord1_pts))
+            
+            # simulated values (datframe to array)
+            simulation_s_data_coord1  = simulation_s_data['coord1'].to_numpy()
+            simulated_smin_values = simulation_s_data['min_s_threshold'].to_numpy()
+            simulated_smax_values = simulation_s_data['max_s_threshold'].to_numpy()
+            
+            # find index of closest value to each point being evaluated, and assign simulated s thresholds
+            pi_p = abs(intrusion_coord1_pts[:,None] - simulation_s_data_coord1[None,:])
+            indexS = np.argmin(pi_p, axis =1)
+            
+            for i in range(len(intrusion_coord0_pts)):
+                s_minside_threshold[i] = simulated_smin_values[indexS[i]]
+                s_maxside_threshold[i] = simulated_smax_values[indexS[i]]
+            
+            # ---- indicator function to define volume of intrusion - w/out considering growth threshold
+            indicator_fxS_boolean = (s_minside_threshold <= intrusion_coord2_pts) * (intrusion_coord2_pts <= s_maxside_threshold)
+            indicator_fxS = indicator_fxS_boolean.astype('int64')
+        
+        if simulation_g_data is None:
+            print('No simultion for vertical extent')
+        else:
+            # containers for thresholds
+            g_minside_threshold = np.zeros(len(intrusion_coord1_pts))
+            g_maxside_threshold = np.zeros(len(intrusion_coord1_pts))
+            
+            # simulated values (dataframe to array)
+            simulation_g_data_coord1  = simulation_g_data['coord1'].to_numpy()
+            simulation_g_data_coord2  = simulation_g_data['coord2'].to_numpy()
+            simulated_gmin_values = simulation_g_data['g_minimum'].to_numpy()
+            simulated_gmax_values = simulation_g_data['g_maximum'].to_numpy()
+            
+            # find index of closest value to each point being evaluated, and assign simulated s thresholds
+            pi_p = abs(intrusion_coord1_pts[:,None] - simulation_g_data_coord1[None,:]) # p_points - p_simulated_data
+            si_s = abs(intrusion_coord2_pts[:,None] - simulation_g_data_coord2[None,:])
+
+            s_min = np.around(si_s/si_s.min(axis = 1)[:,None],2) # flag with 1 the minimum value
+            p_min = np.around(pi_p/pi_p.min(axis = 1)[:,None],2) # flag with 1 the minimum value
+            
+            indexG = np.argmin(abs(1 - (s_min * p_min)), axis = 1)
+
+            for i in range(len(intrusion_coord0_pts)):
+                g_minside_threshold[i] = simulated_gmin_values[indexG[i]]
+                g_maxside_threshold[i] = simulated_gmax_values[indexG[i]]
+
+            indicator_fxG_boolean = (g_minside_threshold <= intrusion_coord0_pts) * (intrusion_coord0_pts <= g_maxside_threshold)
+            indicator_fxG = indicator_fxG_boolean.astype('int64')
+            
+            
+        indicator_fx_boolean = indicator_fxS_boolean * indicator_fxG_boolean
+        indicator_fx = indicator_fx_boolean.astype('int64')
+        
+
+#         ------- intrusion_sf: final distance scalar field
+
+        #containers for distance scalar field of each coordinate
+        intrusion_sf0_temp = np.zeros(len(intrusion_coord0_pts))
+        intrusion_sf1_temp = np.zeros(len(intrusion_coord1_pts))
+        intrusion_sf2_temp = np.zeros(len(intrusion_coord2_pts))
+        intrusion_sf = np.zeros(len(points))
+#         a = (intrusion_coord2_pts>=s_maxside_threshold).astype('int64')
+#         b = ((0 <= intrusion_coord2_pts)*(intrusion_coord2_pts < s_maxside_threshold)).astype('int64')
+#         c = (intrusion_coord2_pts<=s_minside_threshold).astype('int64')
+#         d = ((0 > intrusion_coord2_pts)*(intrusion_coord2_pts > s_minside_threshold)).astype('int64')
+
+#         intrusion_sf2_temp = (intrusion_coord2_pts - s_maxside_threshold)*(intrusion_coord2_pts>=s_maxside_threshold).astype('int64')
+#         + -1*((0 <= intrusion_coord2_pts)*(intrusion_coord2_pts < s_maxside_threshold))
+#         + abs(intrusion_coord2_pts - s_minside_threshold)*(intrusion_coord2_pts<=s_minside_threshold)
+#         +-1*((0 > intrusion_coord2_pts)*(intrusion_coord2_pts > s_minside_threshold))
+        
+        for i in range(len(points)): #strike scalar field
+            if intrusion_coord2_pts[i] >= s_maxside_threshold[i]:
+                intrusion_sf2_temp[i] = (intrusion_coord2_pts[i] - s_maxside_threshold[i])
+            elif intrusion_coord2_pts[i] <= s_minside_threshold[i]:
+                intrusion_sf2_temp[i] = abs(intrusion_coord2_pts[i] - s_minside_threshold[i])
+            elif 0 <= intrusion_coord2_pts[i] < s_maxside_threshold[i]:
+                intrusion_sf2_temp[i] = -1 #(intrusion_coord2_pts[i] - s_maxside_threshold[i])
+            elif 0 > intrusion_coord2_pts[i] > s_minside_threshold[i]:
+                intrusion_sf2_temp[i] = -1 #-(intrusion_coord2_pts[i] - s_minside_threshold[i])
+            else: continue
+       
+            
+        for i in range(len(points)): #growth scalar field
+            if intrusion_coord0_pts[i] < g_minside_threshold[i]:
+                intrusion_sf0_temp[i] = abs(intrusion_coord0_pts[i] - g_minside_threshold[i])
+            elif intrusion_coord0_pts[i] >= g_maxside_threshold[i]:
+                intrusion_sf0_temp[i] = abs(intrusion_coord0_pts[i] - g_maxside_threshold[i])
+            else:
+                intrusion_sf0_temp[i] = -1 #*abs(intrusion_coord0_pts[i])
+
+        intrusion_sf = intrusion_sf0_temp + intrusion_sf2_temp
+        
+        self.evaluated_points = [points, intrusion_coord0_pts, intrusion_coord1_pts, intrusion_coord2_pts]
+        self.intrusion_indicator_function = indicator_fx
+
+        return intrusion_sf #, indicator_fx, intrusion_sf2_temp, intrusion_sf0_temp, 
+
+        
+    def evaluate_value2(self, points):
+    
+        """
+        points : array (x,y,z),  points where the intrusion is evaluated.
+
+        """
+
         simulation_g_data = self.simulation_growth_data
         simulation_s_data = self.simulation_lateral_data
         inet_polygon = None
@@ -186,11 +314,11 @@ class IntrusionFeature():
 
         intrusion_sf = intrusion_sf0_temp
 
-        for i in range(len(indicator_fx_intrusion)):
-            if indicator_fx_intrusion[i] == 1:
-                intrusion_sf[i] = -10
-            else:
-                intrusion_sf[i] = 1 + abs(intrusion_coord0_pts[i]) + abs(intrusion_coord1_pts[i]) + abs(intrusion_coord2_pts[i])
+        # for i in range(len(indicator_fx_intrusion)):
+        #     if indicator_fx_intrusion[i] == 1:
+        #         intrusion_sf[i] = -10
+        #     else:
+        #         intrusion_sf[i] = 1 + abs(intrusion_coord0_pts[i]) + abs(intrusion_coord1_pts[i]) + abs(intrusion_coord2_pts[i])
         
         self.evaluated_points = [points, intrusion_coord0_pts, intrusion_coord1_pts, intrusion_coord2_pts]
         self.intrusion_indicator_function = indicator_fx_intrusion
