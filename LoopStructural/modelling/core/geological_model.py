fix: bug with calculating average fault ori

LoopStructural/modelling/core/geological_model.py
@@ -1401,7 +1401,7 @@ class GeologicalModel:
             fault_frame_data["coord"] == 0, ~np.isnan(fault_frame_data["nz"])
         )
         vector_data = np.vstack([vector_data,fault_frame_data.loc[mask2, ["nx", "ny", "nz"]].to_numpy()])
-        fault_normal_vector = np.mean(vector_data,axis=1)
+        fault_normal_vector = np.mean(vector_data,axis=0)
         logger.info(f'Fault normal vector: {fault_normal_vector}')
 
         mask = np.logical_and(
@@ -1433,9 +1433,9 @@ class GeologicalModel:
             fault_slip_vector = dip_vector[:, 0]
             logger.info(f'Estimated fault slip vector: {fault_slip_vector}')
 
-        if fault_center is not None:
+        if fault_center is not None and ~np.isnan(fault_center).any():
             fault_center = self.scale(fault_center, inplace=False)
-        if fault_center is None:
+        else:
             # if we haven't defined a fault centre take the center of mass for lines assocaited with
             # the fault trace
             if (

LoopStructural/modelling/fault/fault_builder.py
@@ -103,7 +103,9 @@ class FaultBuilder(StructuralFrameBuilder):
                 fault_trace[:, None, :] - fault_trace[None, :, :], axis=2
             )
             if len(distance) == 0 or np.sum(distance) == 0:
-                logger.error("There is no fault trace for {}".format(self.name))
+                logger.warning("There is no fault trace for {}".format(self.name))
+                # this can mean there is only a single data point for the fault, its not critical
+                # but probably means the fault isn't well defined. 
                 # add any data anyway - usually just orientation data
                 self.add_data_from_data_frame(data)
                 self.origin = self.model.bounding_box[0, :]