fix: interpolated INet, removal of point scaling

LoopStructural/modelling/intrusions/intrusion_network.py
@@ -440,11 +440,12 @@ class IntrusionNetwork:
         elif self.intrusion_network_type == "interpolated":
 
             inet_points = np.zeros([len(self.intrusion_network_data), 4])
-            inet_points_scaled = self.intrusion_network_data.loc[
+            inet_points_xyz = self.intrusion_network_data.loc[
                 :, ["X", "Y", "Z"]
             ].to_numpy()
-            inet_points_rescaled = self.model.rescale(inet_points_scaled)
-            inet_points[:, :3] = inet_points_rescaled
+            # inet_points_scaled = self.model.scale(inet_points_xyz, inplace = False)
+
+            inet_points[:, :3] = inet_points_xyz
 
             self.intrusion_network_outcome = inet_points
             return inet_points