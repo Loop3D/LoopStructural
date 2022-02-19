0574e57bc639fe809b3e9b02531ce0e10742ea36
Lachlan Grose <lachlan.grose@gmail.com>
2022-02-19 13:12:06 +1100

    docs: :memo: adding warning that analysis module is experimental

--- LoopStructural/analysis/__init__.py
+++ LoopStructural/analysis/__init__.py
@@ -4,6 +4,7 @@ Analysis
 
 Various tools for analysing loopstructural models, including calculating fault intersections and fault toplogies
 """
+logger.warning("LoopStructural.analysis is experimental and may not perform as expected")
 from ._fault_displacement import displacement_missfit
 from ._fault_intersection import calculate_fault_intersections
 from ._topology import calculate_fault_topology_matrix
