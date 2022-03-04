5c68b01e6d43b302696f3b60bea220fbe4a192f5
Lachlan Grose <lachlan.grose@monash.edu>
2022-03-04 14:42:12 +1100

    refactor: changing import * to explicit import

--- LoopStructural/modelling/intrusions/__init__.py
+++ LoopStructural/modelling/intrusions/__init__.py
@@ -1,3 +1,3 @@
 from .intrusion_feature import IntrusionFeature
 from .intrusion_builder import IntrusionBuilder
-from .geom_conceptual_models import *
+from .geom_conceptual_models import ellipse_function, rectangle_function, parallelepiped_function, obliquecone_function
