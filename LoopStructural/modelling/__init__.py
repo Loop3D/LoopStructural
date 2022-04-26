5db6a8113d5dd0f6f2c3c59078cbd812f4aecafb
Lachlan Grose <lachlan.grose@gmail.com>
2022-04-26 16:51:51 +1000

    fix: :ambulance: try 2, catching loopimporterror as well as import error

--- LoopStructural/modelling/__init__.py
+++ LoopStructural/modelling/__init__.py
@@ -3,13 +3,13 @@ Geological modelling classes and functions
 
 """
 from LoopStructural.utils import getLogger
-
+from LoopStructural.utils import LoopImportError
 logger = getLogger(__name__)
 from LoopStructural.modelling.input.process_data import ProcessInputData
 
 try:
     from LoopStructural.modelling.input.project_file import LoopProjectfileProcessor
-except ImportError:
+except LoopImportError, ImportError:
     logger.error("Loop project file cannot be imported")
 from LoopStructural.modelling.features import StructuralFrame
 from LoopStructural.modelling.features import StructuralFrameBuilder
