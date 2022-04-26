34fb1d695b101cfbefbcb5f8e8bbb0449e170c18
Lachlan Grose <lachlan.grose@gmail.com>
2022-04-26 16:47:11 +1000

    fix: :ambulance: quick fix for projectfile import error

--- LoopStructural/modelling/__init__.py
+++ LoopStructural/modelling/__init__.py
@@ -2,9 +2,15 @@
 Geological modelling classes and functions
 
 """
+from LoopStructural.utils import getLogger
 
+logger = getLogger(__name__)
 from LoopStructural.modelling.input.process_data import ProcessInputData
-from LoopStructural.modelling.input.project_file import LoopProjectfileProcessor
+
+try:
+    from LoopStructural.modelling.input.project_file import LoopProjectfileProcessor
+except ImportError:
+    logger.error("Loop project file cannot be imported")
 from LoopStructural.modelling.features import StructuralFrame
 from LoopStructural.modelling.features import StructuralFrameBuilder
 from LoopStructural.modelling.fault import FaultBuilder
