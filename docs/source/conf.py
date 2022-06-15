6582d0c8381588e8f59c623641c7d8ce451891fc
Lachlan Grose <lachlan.grose@gmail.com>
2022-06-15 16:10:10 +1000

    docs: trying to ignore cython

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -95,7 +95,9 @@ html_theme = 'pydata_sphinx_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
+autosummary_mock_imports = [
+    'LoopStructural.interpolators._cython',
+]
 # Sphinx gallery examples
 from LoopStructural.visualisation.sphinx_scraper import Scraper as LoopScraper
 from sphinx_gallery.sorting import ExampleTitleSortKey
