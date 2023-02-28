e720e12715b1f687562455a5151e2f230c198da0
Lachlan Grose <lachlan.grose@monash.edu>
2023-02-28 16:11:58 +1100

    doc: define classes to document

--- LoopStructural/modelling/intrusions/__init__.py
+++ LoopStructural/modelling/intrusions/__init__.py
@@ -6,4 +6,20 @@ from .geom_conceptual_models import (
     constant_function,
     obliquecone_function,
 )
-from .geometric_scaling_functions import *
+from .geometric_scaling_functions import (
+    geometric_scaling_parameters,
+    thickness_from_geometric_scaling,
+    contact_pts_using_geometric_scaling,
+)
+
+__all__ = [
+    "IntrusionFeature",
+    "IntrusionFrameBuilder",
+    "IntrusionBuilder",
+    "ellipse_function",
+    "constant_function",
+    "obliquecone_function",
+    "geometric_scaling_parameters",
+    "thickness_from_geometric_scaling",
+    "contact_pts_using_geometric_scaling",
+]
