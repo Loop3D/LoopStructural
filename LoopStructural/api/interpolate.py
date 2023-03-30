f9709b54515b97cee9eed429110cbb5101edd24c
Lachlan Grose <lachlan.grose@monash.edu>
2023-03-30 16:17:59 +1100

    fix: adding placeholder for interpolate api

--- LoopStructural/api/interpolate.py
+++ LoopStructural/api/interpolate.py
@@ -1,1 +1,13 @@
+import numpy as np
+import pandas as pd
+
+from typing import Optional
 from LoopStructural.interpolators import PiecewiseLinearInterpolator
+
+
+def interpolate_property(
+    value_points: Optional[np.ndarray] = None,
+    norm_constraints: Optional[np.ndarray] = None,
+    interpolatortype="FDI",
+):
+    pass
