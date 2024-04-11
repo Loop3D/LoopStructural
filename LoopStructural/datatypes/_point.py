5a61e0b5dc52bf7ee0b9b44b44a674cafa49a387
Lachlan Grose <lachlan.grose@monash.edu>
2024-04-12 09:21:09 +1000

    adding save functions for pointsets, backends not written for geoh5 and gocad

--- LoopStructural/datatypes/_point.py
+++ LoopStructural/datatypes/_point.py
@@ -45,3 +45,31 @@ class VectorPoints:
 
         # Perform the glyph
         return points.glyph(orient="vectors", geom=geom)
+
+    def save(self, filename):
+        filename = str(filename)
+        ext = filename.split('.')[-1]
+        if ext == 'json':
+            import json
+
+            with open(filename, 'w') as f:
+                json.dump(self.to_dict(), f)
+        elif ext == 'vtk':
+            self.vtk().save(filename)
+
+        elif ext == 'geoh5':
+            from LoopStructural.export.geoh5 import add_points_to_geoh5
+
+            add_points_to_geoh5(filename, self)
+        elif ext == 'pkl':
+            import pickle
+
+            with open(filename,
+                        'wb') as f:
+                    pickle.dump(self, f)
+        elif ext == 'vs':
+            from LoopStructural.export.gocad import _write_pointset
+            _write_pointset(self, filename)
+        else:
+            raise ValueError(f'Unknown file extension {ext}')
+        
