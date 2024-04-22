d323e78a9b0b0df82d15eaf22ad7a58122499b6c
Lachlan Grose <lachlan.grose@monash.edu>
2024-04-22 10:01:46 +1000

    updatingtests

--- tests/unit/datatypes/test__structured_grid.py
+++ tests/unit/datatypes/test__structured_grid.py
@@ -10,7 +10,9 @@ def test_structured_grid_to_dict():
     data = np.random.rand(10, 10, 10)
     name = "grid_data"
 
-    grid = StructuredGrid(origin, nsteps, step_vector, data, name)
+    grid = StructuredGrid(
+        origin=origin, step_vector=step_vector, nsteps=nsteps, data=data, name=name
+    )
     grid_dict = grid.to_dict()
 
     assert np.all(grid_dict["origin"] == origin)
@@ -25,7 +27,7 @@ def test_structured_grid_maximum():
     nsteps = np.array([10, 10, 10])
     step_vector = np.array([1, 1, 1])
 
-    grid = StructuredGrid(origin, nsteps, step_vector, None, None)
+    grid = StructuredGrid(origin, step_vector, nsteps, None, None)
     maximum = grid.maximum
 
     expected_maximum = origin + nsteps * step_vector
@@ -43,12 +45,19 @@ def test_structured_grid_vtk():
     data = np.random.rand(10, 10, 10)
     name = "grid_data"
 
-    grid = StructuredGrid(origin, nsteps, step_vector, data, name)
+    grid = StructuredGrid(origin, step_vector, nsteps, data, name)
     vtk_grid = grid.vtk
+    grid_points = vtk_grid.points
+    grid_origin = np.min(grid_points, axis=0)
 
     # Add assertions to validate the generated vtk_grid
     assert isinstance(vtk_grid, pv.RectilinearGrid)
     assert np.array_equal(vtk_grid.dimensions, nsteps)
-    assert np.array_equal(vtk_grid.origin, origin)
-    assert np.array_equal(vtk_grid.spacing, step_vector)
+    assert np.array_equal(grid_origin, origin)
     assert np.array_equal(vtk_grid[name], data.flatten(order="F"))
+
+
+if __name__ == "__main__":
+    test_structured_grid_to_dict()
+    test_structured_grid_maximum()
+    test_structured_grid_vtk()
