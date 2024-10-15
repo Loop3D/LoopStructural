778eb6a6e46bd29b8c17cd8a97af278414c1c74d
Lachlan Grose <lachlan.grose@monash.edu>
2024-10-15 17:18:40 +1100

    getter/factory for building fold profile object

--- LoopStructural/modelling/features/fold/fold_function/__init__.py
+++ LoopStructural/modelling/features/fold/fold_function/__init__.py
@@ -1,6 +1,9 @@
 from ._trigo_fold_rotation_angle import TrigoFoldRotationAngleProfile
 from ._fourier_series_fold_rotation_angle import FourierSeriesFoldRotationAngleProfile
 from enum import Enum
+from typing import Optional
+import numpy.typing as npt
+import numpy as np
 
 
 class FoldRotationType(Enum):
@@ -13,3 +16,12 @@ class FoldRotationType(Enum):
 
     def __repr__(self):
         return self.name
+
+
+def get_fold_rotation_profile(
+    fold_rotation_type,
+    rotation_angle: Optional[npt.NDArray[np.float64]] = None,
+    fold_frame_coordinate: Optional[npt.NDArray[np.float64]] = None,
+    **kwargs,
+):
+    return fold_rotation_type.value(rotation_angle, fold_frame_coordinate, **kwargs)
