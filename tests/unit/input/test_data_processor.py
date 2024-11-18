4a2570223d2971158b4940464e61c8e7edc88952
Lachlan Grose <lachlan.grose@monash.edu>
2024-11-18 15:23:58 +1100

    tests: updating processor test

--- tests/unit/input/test_data_processor.py
+++ tests/unit/input/test_data_processor.py
@@ -7,7 +7,7 @@ import numpy as np
 def test_create_processor():
     df = pd.DataFrame(rng.random(size=(10, 3)), columns=["X", "Y", "Z"])
     df["name"] = ["unit_{}".format(name % 2) for name in range(10)]
-    stratigraphic_order = [("sg", ["unit_0", "unit_1"])]
+    stratigraphic_order = [("sg", ["unit_0", "unit_1", "basement"])]
     thicknesses = {"unit_0": 1.0, "unit_1": 0.5}
     processor = ProcessInputData(
         contacts=df, stratigraphic_order=stratigraphic_order, thicknesses=thicknesses
