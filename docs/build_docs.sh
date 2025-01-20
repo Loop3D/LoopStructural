1e275f9e78e1942a7ec9b88623f5da70dc68dfaa
Lachlan Grose <lachlan.grose@monash.edu>
2025-01-20 11:00:16 +1100

    docs: adding loopstructuralvisualisation[all] and removing xvfb stuff

--- docs/build_docs.sh
+++ docs/build_docs.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-set -x
-export DISPLAY=:99.0
-export PYVISTA_OFF_SCREEN=true
-which Xvfb
-Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
-sleep 3
-set +x
-exec "$@"
+# set -x
+# export DISPLAY=:99.0
+# export PYVISTA_OFF_SCREEN=true
+# which Xvfb
+# Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
+# sleep 3
+# set +x
+# exec "$@"
 pip install ./LoopStructural
+pip install loopstructuralvisualisation[all]
 make -C LoopStructural/docs html
