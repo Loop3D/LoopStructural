fix: updating scaling for plotting (#219)
* docs: add pyvista plot directives and update some documentations

* fix: interpolatorbuilder syntax/method naming

* fix: bb global origin should be 0 if it is not being used.

force origin/max to be numpy arrays.
Change default ordering of regular grid to be fortran
renaming centers to centres for english
adding cell centres to structure grid/nodes

* fix: allow fitting of rotation to be disabled for the euclidean transformation and ensure dimensionality is used

* update copy of points

Co-authored-by: Copilot <175728472+Copilot@users.noreply.github.com>

* Add Optional back

Co-authored-by: Copilot <175728472+Copilot@users.noreply.github.com>

* fix: geoh5 format for grids

* fix: adding spatially varying regularisation for fdi

* fix: interpolator builder use bounding box geometry if no other mesh details are given

* style: style fixes by ruff and autoformatting by black

* fix: change nelements for interpolator using builder kwargs

* style: black

* style: remove unused imports

* fix: allowing nelements to be updated for an interpolator

* fix: allow small structured grid. needed for tetra

* fix: adding distance to bounding box

* style: style fixes by ruff and autoformatting by black

* style: style fixes by ruff and autoformatting by black

* fix: don't scale fdi regularisation

* fix: add helper to get interpolator support as vtk with solution as node values

* fix: if interpolation geometry changed, make sure build args are updated

* style: style fixes by ruff and autoformatting by black

* style: black autoformat

* style: style fixes by ruff and autoformatting by black

* tests: add fdi test for structural frame and make isclose less sensitive

* tests: reducing sensitivity further...

* fix: scaling vectors for visualisation

* fix: copy interpolated feature

* fix: add build args accessor/settor for structural frame

* style: style fixes by ruff and autoformatting by black

---------

Co-authored-by: Copi

LoopStructural/datatypes/_point.py
@@ -129,9 +129,9 @@ class VectorPoints:
     def vtk(
         self,
         geom='arrow',
-        scale=0.10,
+        scale=1.0,
         scale_function=None,
-        normalise=True,
+        normalise=False,
         tolerance=0.05,
         bb=None,
         scalars=None,
@@ -140,9 +140,15 @@ class VectorPoints:
 
         _projected = False
         vectors = np.copy(self.vectors)
+
         if normalise:
             norm = np.linalg.norm(vectors, axis=1)
             vectors[norm > 0, :] /= norm[norm > 0][:, None]
+        else:
+            norm = np.linalg.norm(vectors, axis=1)
+            vectors[norm > 0, :] /= norm[norm > 0][:, None]
+            norm = norm[norm > 0] / norm[norm > 0].max()
+            vectors *= norm[:, None]
         if scale_function is not None:
             # vectors /= np.linalg.norm(vectors, axis=1)[:, None]
             vectors *= scale_function(self.locations)[:, None]
@@ -151,6 +157,7 @@ class VectorPoints:
             try:
                 locations = bb.project(locations)
                 _projected = True
+                scale = bb.scale_by_projection_factor(scale)
             except Exception as e:
                 logger.error(f'Failed to project points to bounding box: {e}')
                 logger.error('Using unprojected points, this may cause issues with the glyphing')
@@ -161,10 +168,10 @@ class VectorPoints:
         if geom == 'arrow':
             geom = pv.Arrow(scale=scale)
         elif geom == 'disc':
-            geom = pv.Disc(inner=0, outer=scale).rotate_y(90)
+            geom = pv.Disc(inner=0, outer=scale * 0.5, c_res=50).rotate_y(90)
 
         # Perform the glyph
-        glyphed = points.glyph(orient="vectors", geom=geom, tolerance=tolerance, scale=False)
+        glyphed = points.glyph(orient="vectors", geom=geom, tolerance=tolerance)
         if _projected:
             glyphed.points = bb.reproject(glyphed.points)
         return glyphed