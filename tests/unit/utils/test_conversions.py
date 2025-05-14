1c01d196c1cd5202e686e8c24b58fb8370f6053b
Lachlan Grose <lachlan.grose@monash.edu>
2025-05-14 13:30:31 +1000

    fix: update tests

--- tests/unit/utils/test_conversions.py
+++ tests/unit/utils/test_conversions.py
@@ -1,4 +1,4 @@
-from LoopStructural.utils import strikedip2vector, azimuthplunge2vector
+from LoopStructural.utils import strikedip2vector, plungeazimuth2vector
 import numpy as np
 
 
@@ -19,18 +19,18 @@ def test_strikedip2vector():
 
 
 # import numpy as np
-# from LoopStructural.utils.maths import azimuthplunge2vector
+# from LoopStructural.utils.maths import plungeazimuth2vector
 
 
-def test_azimuthplunge2vector_single_values():
+def test_plungeazimuth2vector_single_values():
     plunge = 0
     plunge_dir = 90
     expected_result = np.array([[1, 0, 0]])
-    result = azimuthplunge2vector(plunge, plunge_dir)
+    result = plungeazimuth2vector(plunge, plunge_dir)
     assert np.allclose(result, expected_result)
 
 
-def test_azimuthplunge2vector_array_values():
+def test_plungeazimuth2vector_array_values():
     plunge = [0, 90, 0]
     plunge_dir = [90, 90, 0]
     expected_result = np.array(
@@ -40,12 +40,12 @@ def test_azimuthplunge2vector_array_values():
             [0, 1, 0],
         ]
     )
-    result = azimuthplunge2vector(plunge, plunge_dir)
+    result = plungeazimuth2vector(plunge, plunge_dir)
     assert np.allclose(result, expected_result)
 
 
-def test_azimuthplunge2vector_empty_arrays():
+def test_plungeazimuth2vector_empty_arrays():
     plunge = []
     plunge_dir = []
-    result = azimuthplunge2vector(plunge, plunge_dir)
+    result = plungeazimuth2vector(plunge, plunge_dir)
     assert result.shape[0] == 0
