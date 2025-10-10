3d4d90f90ed0dd54db74cf354cf4710fd9f99321
lachlangrose <lachlan.grose@monash.edu>
2025-10-10 12:18:51 +1100

    fix: remove fold rotation inversion when axis and cross product are not in the same direction.

--- LoopStructural/modelling/features/fold/_foldframe.py
+++ LoopStructural/modelling/features/fold/_foldframe.py
@@ -166,7 +166,7 @@ class FoldFrame(StructuralFrame):
             ds = np.einsum("ij,ij->i", fold_axis, vv)
             flr = np.rad2deg(np.arcsin(r2))  # np.where(ds > 0, np.rad2deg(np.arcsin(r2)),
             # (- )))
-            flr[ds < 0] *= -1
+            # flr[ds < 0] *= -1     
 
             # flr = np.where(flr < -90, (180. + flr), flr)
             # flr = np.where(flr > 90, -(180. - flr), flr)
