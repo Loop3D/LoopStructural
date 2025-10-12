c09737245c31a0b18ee6b35d2271edbc672bf939
lachlangrose <lachlan.grose@monash.edu>
2025-10-13 08:30:29 +1100

    fix: adding min/max angle to trig profile

--- LoopStructural/modelling/features/fold/fold_function/_trigo_fold_rotation_angle.py
+++ LoopStructural/modelling/features/fold/fold_function/_trigo_fold_rotation_angle.py
@@ -14,7 +14,8 @@ class TrigoFoldRotationAngleProfile(BaseFoldRotationAngleProfile):
         fold_frame_coordinate: Optional[npt.NDArray[np.float64]] = None,
         origin: float = 0,
         wavelength: float = 0,
-        inflectionpointangle: float = 0,
+        inflectionpointangle_min: float = 0,
+        inflectionpointangle_max: float = 0,
     ):
         """The fold frame function using the trigo profile from Laurent 2016
 
@@ -34,7 +35,8 @@ class TrigoFoldRotationAngleProfile(BaseFoldRotationAngleProfile):
         super().__init__(rotation_angle, fold_frame_coordinate)
         self._origin = origin
         self._wavelength = wavelength
-        self._inflectionpointangle = inflectionpointangle
+        self._inflectionpointangle_min = inflectionpointangle_min
+        self._inflectionpointangle_max = inflectionpointangle_max
 
     @property
     def origin(self):
@@ -43,7 +45,39 @@ class TrigoFoldRotationAngleProfile(BaseFoldRotationAngleProfile):
     @property
     def wavelength(self):
         return self._wavelength
-
+    @property
+    def inflectionpointangle_min(self):
+        return self._inflectionpointangle_min
+    @property
+    def inflectionpointangle_max(self):
+        return self._inflectionpointangle_max
+    @inflectionpointangle_max.setter
+    def inflectionpointangle_max(self, value):
+        if np.isfinite(value):
+            if value < np.deg2rad(-90) or value > np.deg2rad(90):
+                logger.error(f"Inflection point angle max is {np.rad2deg(value)} degrees")
+                raise ValueError("inflectionpointangle_max must be between 0 and 90")
+            self.notify_observers()
+            self._inflectionpointangle_max = value
+        else:
+            raise ValueError("inflectionpointangle_max must be a finite number")
+    @inflectionpointangle_min.setter
+    def inflectionpointangle_min(self, value):
+        if np.isfinite(value):
+            if value < np.deg2rad(-90) or value > np.deg2rad(90):
+                logger.error(f"Inflection point angle min is {np.rad2deg(value)} degrees")
+                raise ValueError("inflectionpointangle_min must be between -90 and 0")
+            self.notify_observers()
+            self._inflectionpointangle_min = value
+        else:
+            raise ValueError("inflectionpointangle_min must be a finite number")
+    @property
+    def inflectionpointangle_half(self):
+        return (self._inflectionpointangle_max - self._inflectionpointangle_min) / 2
+    
+    @property
+    def inflectionpointangle_shift(self):
+        return (self._inflectionpointangle_max + self._inflectionpointangle_min) / 2
     @property
     def inflectionpointangle(self):
         return self._inflectionpointangle
@@ -83,11 +117,12 @@ class TrigoFoldRotationAngleProfile(BaseFoldRotationAngleProfile):
         return {
             "origin": self.origin,
             "wavelength": self.wavelength,
-            "inflectionpointangle": self.inflectionpointangle,
+            "inflectionpointangle_min": self.inflectionpointangle_min,
+            "inflectionpointangle_max": self.inflectionpointangle_max,
         }
 
     @staticmethod
-    def _function(s, origin, wavelength, inflectionpointangle):
+    def _function(s, origin, wavelength, inflectionpointangle_min, inflectionpointangle_max):
         """
 
         Parameters
@@ -101,8 +136,12 @@ class TrigoFoldRotationAngleProfile(BaseFoldRotationAngleProfile):
         -------
 
         """
-        tan_alpha_delta_half = np.tan(inflectionpointangle)
-        tan_alpha_shift = 0
+        inflectionpointangle_half = (inflectionpointangle_max - inflectionpointangle_min) / 2
+        inflectionpointangle_shift = (inflectionpointangle_max + inflectionpointangle_min) / 2
+        tan_alpha_delta_half = np.tan(inflectionpointangle_half)
+        tan_alpha_shift = np.tan(inflectionpointangle_shift)
+        print(f"tan_alpha_delta_half {np.rad2deg(np.arctan(tan_alpha_delta_half))} degrees")
+        print(f"tan_alpha_shift {np.rad2deg(np.arctan(tan_alpha_shift))} degrees")
         x = (s - origin) / wavelength
         return tan_alpha_delta_half * np.sin(2 * np.pi * x) + tan_alpha_shift
 
@@ -123,7 +162,8 @@ class TrigoFoldRotationAngleProfile(BaseFoldRotationAngleProfile):
     def update_params(self, params: Union[List, npt.NDArray[np.float64]]) -> None:
         self.origin = params[0]
         self.wavelength = params[1]
-        self.inflectionpointangle = params[2]
+        self.inflectionpointangle_min = params[2]
+        self.inflectionpointangle_max = params[3]
 
     def initial_guess(
         self,
